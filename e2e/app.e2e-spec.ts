1d7c308e4b117b8193a78773db9b14a5c107b42d
jesus Barajas Camacho <jesusbarcam@gmail.com>
2017-09-29 22:58:55 +0200

    Adapted project to Yarn

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { MundilabelPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('mundilabel App', function() {
-  let page: MundilabelPage;
+describe('mundilabel App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new MundilabelPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from 'protractor';
 
-export class MundilabelPage {
+export class AppPage {
   navigateTo() {
     return browser.get('/');
   }
