9a0ccc44f9c0d0f2737bd4a17f56ac49e3f41c03
jesus Barajas Camacho <jesusbarcam@gmail.com>
2017-10-01 12:00:23 +0200

    Created i18n Files English and spanish, and routing module

---
+++ src/app/app.routing.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// import {  }
+
+
+// const mundilabelRoutes: Routes = [
+//   {path: '', pathMatch: 'full', redirectTo: 'public'},
+//   {path: 'aboutAs', component:  },
+//   {path: 'howWeWork',  },
+//   {path: '**', pathMatch: 'full', component: UnknowPageComponent }
+// ];// MundilabelRoutes
+
+@NgModule({
+
+})
+export class MundilabelRouting {
+
+}// MundilabelRouting
