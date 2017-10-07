c4e48f05ee3eb3b75216dc8b8dac67a798d9246c
jesus Barajas Camacho <jesusbarcam@gmail.com>
2017-10-07 20:18:32 +0200

    Control of router changes to change image of wallpaper

--- src/app/components/wallpaper/wallpaper.component.ts
+++ src/app/components/wallpaper/wallpaper.component.ts
@@ -13,8 +13,11 @@ import { MundilabelRoutes } from '../../mundilabel.routing';
 export class WallpaperComponent implements OnInit {
 
 
-  public wallpaperUrl: string;
+  public static readonly TIME_ANIMATIONS_TITLES: number = 650;
 
+  public wallpaperUrl: string;
+  public routeWithoutTitles: string;
+  private titleStartShow: boolean;
 
 
   /**
@@ -29,22 +32,79 @@ export class WallpaperComponent implements OnInit {
     const initRoute: string = (MundilabelRoutes[1].path).replace('/' , '');
     this.wallpaperUrl = mundilabelSettings.MUNDILABEL_WALLPAPERS[ initRoute ];
 
+    this.titleStartShow = false;
+    this.routeWithoutTitles = MundilabelRoutes[1].path;
+
   }// Constructor
 
 
 
 
 
+  /**
+   * @method
+   * @lifecycle
+   */
   ngOnInit() {
-    this.mundilabelService.activatedRoute$
-        .subscribe((activatedRoute) => {
-
-          this.wallpaperUrl = this.mundilabelSettings.MUNDILABEL_WALLPAPERS[ activatedRoute ];
-          this.changeDetection.markForCheck();
-
-        }); // Subscribe
+    this.inicializeControlOfRoutes();
   }// NgOnInit
 
+
+
+
+
+
+
+  /**
+   * @method
+   * @private
+   * @description
+   */
+  private inicializeControlOfRoutes() {
+    // Inicialize observation of actived route
+    // of application
+    this.mundilabelService.activatedRoute$
+    .subscribe((activatedRoute) => {
+      // Hidden titles of view
+      this.titleStartShow = false;
+      // If activatedRouted is route ok
+      if ( activatedRoute !== '' ) {
+        this.activatedNewRoute( activatedRoute );
+        // and activatedRoute is route with titles
+        if ( activatedRoute !== this.routeWithoutTitles ) {
+          this.countDownStartTitleShow();
+        }// If
+      }// If
+
+    }); // Subscribe
+  }// InicializeControlOfRoutes
+
+
+
+
+  /**
+   * @method
+   * @private
+   * @description
+   */
+  private activatedNewRoute(nextRoute: string) {
+    this.wallpaperUrl = this.mundilabelSettings.MUNDILABEL_WALLPAPERS[ nextRoute ];
+    this.changeDetection.markForCheck();
+  }// Inicialize
+
+
+
+  /**
+   * @method
+   * @private
+   * @description
+   */
+  private countDownStartTitleShow() {
+    setTimeout(() => {
+      this.titleStartShow = true;
+      this.changeDetection.markForCheck();
+    }, WallpaperComponent.TIME_ANIMATIONS_TITLES );
+  }// CountDownStartTitleShow
 
 
 
