45efcb888ee53345fd1895fe2e6020c4257225b7
jesus Barajas Camacho <jesusbarcam@gmail.com>
2017-10-14 17:51:12 +0200

    Implemented footer component

--- src/app/commons/mundilabel.settings.ts
+++ src/app/commons/mundilabel.settings.ts
@@ -4,6 +4,14 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MundilabelSettingsService {
 
+  public readonly NAME_COMPANY: string = 'Mundilabel S.A.L';
+  public readonly MAIL_COMPANY: string = 'mundilabel@hotmail.com';
+  public readonly ADDRESS_COMPANY: string = 'Avenida de Castilla 23 - Nave 3';
+  public readonly PHONE_COMPANY: string = '91 648 76 70';
+  public readonly FAX_COMPANY: string = '91 656 17 44';
+  public readonly TOWN_COMPANY: string = 'San Fernando de Henares';
+  public readonly PROVINCE_COMPANY: string = 'Madrid';
+
   public readonly MUNDILABEL_LOGOTYPE_URL: string = '../../assets/images/mundilabel_logo.svg';
   public readonly MUNDILABEL_WALLPAPERS: any = {
     home: '../../assets/images/wallpaper-home.jpg',
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
+import { MundilabelSettingsService } from '../../commons';
+
+
+
 @Component({
   selector: 'footer',
   templateUrl: './footer.component.html',
@@ -8,9 +12,31 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  private currentYear: number;
+  private address: string;
+  private town: string;
+  private mail: string;
+  private phone: string;
+  private fax: string;
+  private province: string;
+  private nameCompany: string;
+
+
+
+  constructor(private applicationSettings: MundilabelSettingsService) {
+  }// Constructor
+
+
 
   ngOnInit() {
+    this.nameCompany = this.applicationSettings.NAME_COMPANY;
+    this.address = this.applicationSettings.ADDRESS_COMPANY;
+    this.town = this.applicationSettings.TOWN_COMPANY;
+    this.mail = this.applicationSettings.MAIL_COMPANY;
+    this.phone = this.applicationSettings.PHONE_COMPANY;
+    this.fax = this.applicationSettings.FAX_COMPANY;
+    this.province = this.applicationSettings.PROVINCE_COMPANY;
+    this.currentYear = new Date().getFullYear();
   }// NgOnInit
 
 }// FooterComponent
