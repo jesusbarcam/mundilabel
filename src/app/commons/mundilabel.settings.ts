d3668ed7fb0800a279ae44cb64e0f9096a623782
jesus Barajas Camacho <jesusbarcam@gmail.com>
2017-10-21 10:30:03 +0200

    Home clients logos implements and home texts

--- src/app/commons/mundilabel.settings.ts
+++ src/app/commons/mundilabel.settings.ts
@@ -12,9 +12,12 @@ export class MundilabelSettingsService {
   public readonly TOWN_COMPANY: string = 'San Fernando de Henares';
   public readonly PROVINCE_COMPANY: string = 'Madrid';
 
+
+
   public readonly MUNDILABEL_LOGOTYPE_URL: string = '../../assets/images/mundilabel_logo.svg';
   public readonly MUNDILABEL_HAND_HOME_IMAGE_URL: string = '../../assets/images/HandMundilabel.jpg';
   public readonly MUNDILABEL_LABEL_IMAGE_URL: string = '../../assets/images/labelMundilabel.jpg';
+
   public readonly MUNDILABEL_WALLPAPERS: any = {
     home: '../../assets/images/wallpaper-home.jpg',
     about: '../../assets/images/wallpaper-about.jpg',
@@ -23,6 +26,12 @@ export class MundilabelSettingsService {
     contact: '../../assets/images/wallpaper-contact.jpg'
   };
 
-
+  public readonly MUNDILABEL_CLIENTS_LOGOS: any = {
+    ups: '../../assets/images/ups_logo.png',
+    loreal: '../../assets/images/loreal_logo.png',
+    ifema: '../../assets/images/ifema-logo.png',
+    seur: '../../assets/images/seur_logo.png',
+    wurth: '../../assets/images/Wurth_Logo.png'
+  };
 
 }// MundilabelSettingsService
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,6 +13,13 @@ export class HomeComponent implements OnInit {
 
   private handImageUrl: string;
   private labelImageUrl: string;
+
+  private upsClientLogoUrl: string;
+  private lorealClientLogoUrl: string;
+  private seurClientLogoUrl: string;
+  private ifemaClientLogoUrl: string;
+  private wurthClientLogoUrl: string;
+
   private phone: string;
   private mail: string;
 
@@ -27,6 +34,12 @@ export class HomeComponent implements OnInit {
     this.phone = this.applicationSettings.PHONE_COMPANY;
     this.handImageUrl = this.applicationSettings.MUNDILABEL_HAND_HOME_IMAGE_URL;
     this.labelImageUrl = this.applicationSettings.MUNDILABEL_LABEL_IMAGE_URL;
+
+    this.upsClientLogoUrl = this.applicationSettings.MUNDILABEL_CLIENTS_LOGOS.ups;
+    this.lorealClientLogoUrl = this.applicationSettings.MUNDILABEL_CLIENTS_LOGOS.loreal;
+    this.wurthClientLogoUrl = this.applicationSettings.MUNDILABEL_CLIENTS_LOGOS.wurth;
+    this.seurClientLogoUrl = this.applicationSettings.MUNDILABEL_CLIENTS_LOGOS.seur;
+    this.ifemaClientLogoUrl = this.applicationSettings.MUNDILABEL_CLIENTS_LOGOS.ifema;
   }// NgOnInit
 
 
